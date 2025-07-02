340f137fb70bbf0a9fd8b7de56e0367e55d6b625
Nathan Conlon <nconlon@pm.me>
2025-07-02 13:58:14 -0500

    feat: accept prompt as command-line argument and handle missing prompt with error
    feat: Add --verbose flag for detailed output

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
+import os
+import sys
+from google import genai
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
 def main():
-    print("Hello from ai-agent!")
+    model_name = "gemini-2.0-flash-001"
+    if len(sys.argv) == 1:
+        print("Please provide a prompt!")
+        sys.exit(1)
+    else:
+        prompt = sys.argv[1]
+
+    response = client.models.generate_content(model=model_name, contents=prompt)
+
+    if "--verbose" in sys.argv:
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
+    
+
+    print(response.text)
+    
 
 
 if __name__ == "__main__":
